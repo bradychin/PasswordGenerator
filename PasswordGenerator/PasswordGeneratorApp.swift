eb53286e880bef0f912811a0f928469ae4089ca6
Brady <92948316+bradychin@users.noreply.github.com>
2026-05-21 10:53:39 -0400

    Initial Commit

---
+++ PasswordGenerator/PasswordGeneratorApp.swift
@@ -0,0 +1,17 @@
+//
+//  PasswordGeneratorApp.swift
+//  PasswordGenerator
+//
+//  Created by Brady Chin on 2026.05.21.
+//
+
+import SwiftUI
+
+@main
+struct PasswordGeneratorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
